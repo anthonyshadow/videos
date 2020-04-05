c7144ba0eee53dc8186cedc4008155598c75eb45
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2020-04-05 13:06:46 -0400

    created search bar and initialized youtube api

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import SearchBar from "./SearchBar"
+import youtube from "../api/youtube"
+
+require("dotenv").config();
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = (term) => {
+    youtube
+      .get("/search", {
+        params: {
+          part: "snippet",
+          type: "video",
+          maxResults: 25,
+          key: `${API_KEY}`,
+          q: term,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.items)
+        this.setState({ videos: response.data.items });
+      });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        Found: {this.state.videos.length} videos
+      </div>
+    );
+  }
+}
+
+export default App;
